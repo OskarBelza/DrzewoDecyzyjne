24a02457c213059d4f86964bcefb94a3288aeb1c
OskarBelza <oskar.belza@edu.uekat.pl>
2025-10-13 14:36:57 +0200

    feat: add methods to count entropy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from load_data import load_data
-from data_stats import count_unique_values, value_per_attribute
+from data_stats import count_unique_values, value_per_attribute, entropy
 
 if __name__ == "__main__":
     data = load_data("data/gielda.txt", separator=",", headline=False)
-    print(data)
-    print(data.dtype)
+    print("Data:", data)
+    print("Data type:", data.dtype)
 
-    print(count_unique_values(data))
-    print(value_per_attribute(data))
+    print("Number of unique values:", count_unique_values(data))
+    print("Values and their counts:", value_per_attribute(data))
+    print("Dataset entropy:", entropy(data))
